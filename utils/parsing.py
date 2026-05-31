7b63fde05da1e3f97a614901379de37cca97564d
Maoake <maoake.terii@gmail.com>
2026-05-31 17:00:11 +0200

    need to make some test on the funcitons skip_line and first_line

--- utils/parsing.py
+++ utils/parsing.py
@@ -27,5 +27,35 @@ class Parsing():
             return True
         return False
 
+    def _first_line(self, line: str, index: int) -> bool:
+        line_strip = line.strip()
+        tab_split = line_strip.split(":")
+        number = int(tab_split[1])
+        if tab_split[0] != "nb_drones":
+            return False
+        if number < 1:
+            return False
+        return True
+
+    def _skip_line(self, line: str) -> bool:
+        line_strip = line.strip()
+        if line_strip == "":
+            return True
+        return False
+
     def parsing_data(self) -> None:
         self._load_data_from_file()
+        index = 0
+        index_com = 1
+        for line in self.data:
+            if self._skip_line(line):
+                index += 1
+                continue
+            if (self._ignore_hashtag(line)):
+                index_com += 1
+                continue
+            if index == 0:
+                if not self._first_line(line, index):
+                    raise ValueError("TEST")
+        # do it something here
+            index += 1
